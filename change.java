65bcc973799dab9346ce81d81d1476871b95c2d3
hoangSnapeV <90324439+hoangSnapeV@users.noreply.github.com>
2022-02-08 15:07:31 +0700

    firstday

---
+++ change.java
@@ -0,0 +1,9 @@
+import javax.tools.Diagnostic;
+
+public class change {
+    public static void main(String[] args) {
+        double fahrenheit = 3;
+        double celsius = (5.0 / 9) * (fahrenheit - 32) * 1.0;
+        System.out.println("Celsius = " + celsius);
+    }
+}
